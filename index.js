10e8a8f06e4b20c043d04b7ee921566643e0adab
unknown <Codetavo28>
2023-09-19 13:21:11 -0400

    portfolio piloto

---
+++ index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+  sr.reveal('#nav-bar', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('#presentation', {
+    duration: 1500,
+    origin: 'left',
+    distance: '-100px'
+  }); 
+
+  window.sr = ScrollReveal();
+  sr.reveal('#title-projects', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.div-projects', {
+    duration: 2500,
+    origin: 'left',
+    distance: '-100px'
+  });
